42744efcbddd7d96040e2d328ddc595a5f8fbc32
bennyunsigned@gmail.com <bennyunsigned@gmail.com>
2026-05-31 19:40:46 +0530

    chore: remove unused user query debugging scripts

--- query_user.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-conn=mysql.connector.connect(host='localhost',user='root',password='Tosh#140695',database='VidyaRoop')
-cur=conn.cursor(dictionary=True)
-cur.execute("SELECT id, name, email, is_activated, created_at FROM Users WHERE email = 'bennyunsigned@gmail.com';")
-print(cur.fetchall())
-cur.close()
-conn.close()
